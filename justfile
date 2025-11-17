293ddea0269ce49bc82cdab4310cc815ed2450d9
Jiří Filip <j.f.ilip@seznam.cz>
2025-11-17 09:59:17 +0100

    Add instrumentation to webapp

---
+++ justfile
@@ -0,0 +1,7 @@
+set shell := ["powershell.exe"]
+
+
+dev:
+    docker compose down
+    docker compose build
+    docker compose up
--- webapp/main.py
+++ webapp/main.py
@@ -1,12 +1,20 @@
 from fastapi import FastAPI
 import uvicorn
+from prometheus_fastapi_instrumentator import Instrumentator
 
 
 app = FastAPI()
+Instrumentator().instrument(app).expose(app)
+
 
 @app.get("/")
 def index() -> str:
     return "Hello world"
+
+
+@app.get("/dict")
+def return_dict() -> dict[str, str]:
+    return {"hello": "world", "bla": "ble", "bli": "blu"}
 
 
 if __name__ == "__main__":
